deed1f82e7771ff674ab6a629f94bc407ab3289d
Irene González Cantera <irenegcantera@gmail.com>
2022-01-31 23:25:21 +0100

    Merge feature login-messages into develop

--- src/views/login/registrar.php
+++ src/views/login/registrar.php
@@ -4,6 +4,7 @@ use Irene\TiendaOnlineMvc\conf\Configuration;
 
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,6 +12,7 @@ use Irene\TiendaOnlineMvc\conf\Configuration;
     <title>Registrar</title>
     <link href="<?=Configuration::$PATH_LOCALHOST."css/login.css"?>" rel="stylesheet" type="text/css">
 </head>
+
 <body>
     <div id = "login">
         <form method="POST" action="<?php Configuration::$PATH_LOCALHOST.'index.php'?>">
